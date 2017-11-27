bbd96e7ab482d1b69f4e5861591b6dd1ab062fee
Stefano Carrazza <stefano.carrazza@cern.ch>
2017-11-27 18:30:57 +0100

    forcing cython code to use python2 while python code in pyx is adjusted to use python3 syntax

--- integer_points.pyx
+++ integer_points.pyx
@@ -1,3 +1,4 @@
+# cython: language_level=2
 r"""Riemann Theta Integer Points :mod:`abelfunctions.riemanntheta.integer_points`
 =============================================================================
 
@@ -319,7 +320,7 @@ def _find_int_points_python(g, R, T, c, start):
     # construct the integer points when the final dimension is reached
     if g == 0:
         points = numpy.array([], dtype=numpy.double)
-        for i in range(a, b+1):
+        for i in range(int(a), int(b+1)):
             # this algorithm works backwards on the coordinates: the last
             # coordinate found is n1 if our coordinates are {n1,n2,...,ng}
             points = numpy.append(numpy.append([i],start), points)
@@ -330,7 +331,7 @@ def _find_int_points_python(g, R, T, c, start):
     newT = T[:(newg+1),:(newg+1)]
     newTinv = numpy.linalg.inv(newT)
     pts = []
-    for n in range(a, b+1):
+    for n in range(int(a), int(b+1)):
         chat = c[:newg+1]
         that = T[:newg+1,g]
         newc = (chat.T - numpy.dot(newTinv, that)*(n - c[g])).T
@@ -367,6 +368,6 @@ def integer_points_python(g, R, T):
     c = numpy.zeros((g,1))
     points = _find_int_points_python(g-1, R, T, c, [])
     points = numpy.array(points, dtype=numpy.double)
-    N = len(points)/g
+    N = len(points)//g
     points.resize((N,g))
     return points
